80627ea2087367342e77e18cbc59a56c99cf7c2d
Muhammad Talha <taltude49@gmail.com>
2024-02-09 16:34:18 +0500

    complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,48 @@
-import { useState, useEffect } from 'react'
 
-import Navbar from './components/Navbar'
-import Hero from './components/Hero'
-import Service from './components/Service'
-import About from './components/About'
-import Accomplishments from './components/Accomplishments'
-import Projects from './components/Projects'
-import Footer from './components/Footer'
+import { useState, useEffect } from 'react';
 
-import Cursor from './components/Cursor'
-import ScrollToTop from './components/ScrollToTop'
-import Preloader from './components/Preloader'
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import Service from './components/Service';
+import About from './components/About';
+import Accomplishments from './components/Accomplishments';
+import Projects from './components/Projects';
+import Footer from './components/Footer';
 
-import AOS from 'aos'
-import 'aos/dist/aos.css'
+import Cursor from './components/Cursor';
+import ScrollToTop from './components/ScrollToTop';
+import Preloader from './components/Preloader';
+
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 const App = () => {
-
-  const [loading, setLoading] = useState(false)
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const handleLoad = () => {
-      setLoading(false)
-    }
-    if (document.readyState !== 'complete') {
-      setLoading(false)
-      window.addEventListener('load', handleLoad);
+      setLoading(false);
+    };
+
+    if (document.readyState === 'complete') {
+      // If the document is already complete, trigger handleLoad immediately
+      handleLoad();
     } else {
-      setLoading(true)
+      // If the document is not yet complete, add an event listener for the 'load' event
+      window.addEventListener('load', handleLoad);
     }
+
     return () => {
       window.removeEventListener('load', handleLoad);
-    }
-  }, [])
+    };
+  }, []);
 
   useEffect(() => {
-    AOS.init()
-  }, [])
-  
-  
-  return loading
-    ? <Preloader />
-    : <>
+    AOS.init();
+  }, []);
+
+  return loading ? <Preloader /> : (
+    <>
       <Cursor />
       <Navbar />
       <Hero />
@@ -52,7 +53,8 @@ const App = () => {
       <Footer />
       <ScrollToTop />
     </>
+  );
+};
 
-}  
+export default App;
 
-export default App
