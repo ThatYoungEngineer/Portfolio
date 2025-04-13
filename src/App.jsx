6714294d28dcfe67c749fb96561c3af348bd7111
ThatYoungengineer <taltude49@gmail.com>
2025-04-13 17:57:42 +0500

    fix preloader

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,64 @@
-import { lazy, Suspense, useEffect } from 'react'
+import { lazy, Suspense, useEffect, useState } from 'react'
 import AOS from 'aos'
 import 'aos/dist/aos.css'
 
 import Preloader from './components/Preloader'
 
-const App = () => {
+const Cursor = lazy(() => import('./components/Cursor'))
+const Navbar = lazy(() => import('./components/Navbar'))
+const Hero = lazy(() => import('./components/Hero'))
+const Service = lazy(() => import('./components/Service'))
+const About = lazy(() => import('./components/About'))
+const Accomplishments = lazy(() => import('./components/Accomplishments'))
+const Projects = lazy(() => import('./components/Projects'))
+const Footer = lazy(() => import('./components/Footer'))
+const ScrollToTop = lazy(() => import('./components/ScrollToTop'))
 
-  const Cursor = lazy(() => import('./components/Cursor'))
-  const Navbar = lazy(() => import('./components/Navbar'))
-  const Hero = lazy(() => import('./components/Hero'))
-  const Service = lazy(() => import('./components/Service'))
-  const About = lazy(() => import('./components/About'))
-  const Accomplishments = lazy(() => import('./components/Accomplishments'))
-  const Projects = lazy(() => import('./components/Projects'))
-  const Footer = lazy(() => import('./components/Footer'))
-  const ScrollToTop = lazy(() => import('./components/ScrollToTop'))
+const App = () => {
+  const [isPageLoaded, setIsPageLoaded] = useState(false)
+  const [hidePreloader, setHidePreloader] = useState(false)
+
+  console.log('hidePreloader: ', hidePreloader)
 
   useEffect(() => {
     AOS.init()
+
+    const handlePageLoad = () => {
+      setIsPageLoaded(true)
+
+      // wait for animation to finish (1s delay here, same as CSS animation)
+      setTimeout(() => {
+        setHidePreloader(true)
+      }, 1000)
+    }
+
+    if (document.readyState === 'complete') {
+      handlePageLoad()
+    } else {
+      window.addEventListener('load', handlePageLoad)
+    }
+
+    return () => {
+      window.removeEventListener('load', handlePageLoad)
+    }
   }, [])
 
   return (
-    <Suspense fallback={<Preloader />} >
-      <Cursor />
-      <Navbar />
-      <Hero />
-      <Service />
-      <About />
-      <Accomplishments />
-      <Projects />
-      <Footer />
-      <ScrollToTop />
-    </Suspense>
-  );
-};
+    <>
+      {!hidePreloader && <Preloader animateOut={isPageLoaded} />}
+      <Suspense fallback={null}>
+        <Cursor />
+        <Navbar />
+        <Hero />
+        <Service />
+        <About />
+        <Accomplishments />
+        <Projects />
+        <Footer />
+        <ScrollToTop />
+      </Suspense>
+    </>
+  )
+}
 
-export default App;
-
+export default App
--- src/components/Preloader.jsx
+++ src/components/Preloader.jsx
@@ -1,3 +1,4 @@
+import PropTypes from 'prop-types'
 import Box from '@mui/material/Box'
 import LinearProgress from '@mui/material/LinearProgress'
 import { createTheme, ThemeProvider } from '@mui/material/styles'
@@ -8,12 +9,16 @@ const theme = createTheme({
         main: '#ccc',
       },
     },
-  });
+});
 
-const Preloader = () => {
+Preloader.propTypes = {
+  animateOut: PropTypes.bool.isRequired,
+}
+
+const Preloader = ({ animateOut }) => {
     return (
         <ThemeProvider theme={theme}>
-            <main className="w-screen h-screen overflow-hidden bg-[#1a1a1a] flex items-center justify-center">
+            <main className={`fixed top-0 left-0 w-screen h-screen ${animateOut && '-translate-y-full'} overflow-hidden bg-[#1a1a1a] flex items-center justify-center z-[99999999] transition-transform ease-in-out duration-1000`}>
                 <section className='flex flex-col gap-16 items-center justify-center w-screen sm_tablet:w-1/2'>
                     <div id='content__head'> 
                         <h2 className='font-Poppins-Medium text-7xl sm_desktop:text-9xl'>Loading</h2>
